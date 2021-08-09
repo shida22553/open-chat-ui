c6a89e59ef442fcbeb2c55f5b976b8ba3baad96c
shida22553 <53365248+shida22553@users.noreply.github.com>
2021-08-09 22:42:18 +0900

    send message

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ new Vue({
       userName: 'NONAME',
       rooms: [],
       selectedRoomId: null,
-      roomName: null
+      roomName: null,
+      content: null,
+      messages: [],
+      mode: 'user-name-form'
     }
   },
   async mounted() {
@@ -29,7 +32,6 @@ new Vue({
     } else {
       await instance.get('/users/hash')
         .then(function (response) {
-          console.log(response)
           self.userId = response.data.hash
           Cookies.set('user_id', response.data.hash)
         })
@@ -50,6 +52,12 @@ new Vue({
       })
       .catch(function (error) {
       })
+
+    // load messages
+    await self.loadLastMessage()
+    const element = document.getElementById('messages');
+    const bottom = element.scrollHeight - element.clientHeight;
+    element.scroll(0, bottom);
   },
   methods: {
     async createRoom() {
@@ -64,11 +72,69 @@ new Vue({
         })
         .catch(function (error) {
         })
+      await self.loadLastMessage()
+      const element = document.getElementById('messages');
+      const bottom = element.scrollHeight - element.clientHeight;
+      element.scroll(0, bottom);
+      this.mode = 'chat-form'
     },
-    saveUserName(){
+    saveUserName() {
       if (this.userName !== '') {
         Cookies.set('user_name', this.userName)
       }
+      this.mode = 'chat-form'
+    },
+    async createMessage() {
+      const self = this
+      if (self.userName === '') {
+        self.userName = 'NONAME'
+      }
+      const currentLastMessage = self.messages[self.messages.length - 1]
+      if (currentLastMessage != null) {
+        await self.loadMessageAfter(currentLastMessage.id)
+      }
+      await instance.post(`/rooms/${self.selectedRoomId}/messages`, {
+        user_id: self.userId,
+        user_name: self.userName,
+        content: self.content,
+        content_type: 0
+      })
+        .then(function (response) {
+          self.content = ''
+          self.messages.push(response.data)
+        })
+        .catch(function (error) {
+        })
+      const lastMessage = self.messages[self.messages.length - 1]
+      await self.loadMessageAfter(lastMessage.id)
+    },
+    async loadLastMessage() {
+      const self = this
+      await instance.get(`/rooms/${self.selectedRoomId}/messages`)
+        .then(function (response) {
+          self.messages = response.data.reverse()
+        })
+        .catch(function (error) {
+        })
+    },
+    async loadMessageBefore() {
+      const self = this
+      const nextMessageId = self.messages[0].id
+      await instance.get(`/rooms/${self.selectedRoomId}/messages?next_message_id=${nextMessageId}`)
+        .then(function (response) {
+          self.messages.splice(0, 0, ...response.data.reverse())
+        })
+        .catch(function (error) {
+        })
+    },
+    async loadMessageAfter(prevMessageId) {
+      const self = this
+      await instance.get(`/rooms/${self.selectedRoomId}/messages?prev_message_id=${prevMessageId}`)
+        .then(function (response) {
+          self.messages.splice(self.messages.length, 0, ...response.data.reverse())
+        })
+        .catch(function (error) {
+        })
     }
   },
   computed: {
