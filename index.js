ac47ac2b31a058ed88509fe1d52b4e7440ffbc6e
shida22553 <53365248+shida22553@users.noreply.github.com>
2021-08-09 19:12:27 +0900

    create room

--- index.js
+++ index.js
@@ -1,57 +1,49 @@
-// var app = new Vue({
-//   el: '#app',
-//   data() {
-//     return {
-//       intervalId: null,
-//       inputIntervalMinutes: 15,
-//       intervalMinutes: 15,
-//       seconds: 15 * 60,
-//       isActive: false,
-//       seAudio: null,
-//     }
-//   },
-//   mounted() {
-//     this.seAudio = new Audio('se.mp3')
-//   },
-//   methods: {
-//     tick() {
-//       this.seconds -= 1
-//       if (this.seconds < 0) {
-//         this.playSe()
-//         this.reset()
-//       }
-//     },
-//     start() {
-//       this.intervalId = setInterval(() => {
-//         this.tick()
-//       }, 1000)
-//       this.isActive = true
-//     },
-//     stop() {
-//       clearInterval(this.intervalId)
-//       this.isActive = false
-//     },
-//     setIntervalSeconds() {
-//       this.stop()
-//       this.intervalMinutes = this.inputIntervalMinutes
-//       this.reset()
-//     },
-//     reset() {
-//       this.seconds = this.intervalMinutes * 60
-//     },
-//     playSe() {
-//       this.seAudio.currentTime = 0
-//       this.seAudio.play()
-//     }
-//   },
-//   computed: {
-//     timeStr() {
-//       const minutes = ('0' + Math.floor(this.seconds / 60)).slice(-2)
-//       const seconds = ('0' + this.seconds % 60).slice(-2)
-//       return `${minutes}:${seconds}`
-//     },
-//     inputIntervalMinutesValid() {
-//       return this.inputIntervalMinutes !== '' && this.inputIntervalMinutes < 100
-//     }
-//   }
-// })
+const instance = axios.create({
+  baseURL: 'https://evening-ravine-22261.herokuapp.com/v1/',
+  timeout: 1000,
+  headers: {
+    'Content-Type': 'application/json',
+    'X-Requested-With': 'XMLHttpRequest'
+  },
+  responseType: 'json'  
+})
+
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      rooms: [],
+      selectedRoomId: null,
+      roomName: null
+    }
+  },
+  async mounted() {
+    const self = this
+    await instance.get('/rooms')
+      .then(function (response) {
+        self.rooms = response.data
+        if (self.rooms.length == 0) return
+        self.selectedRoomId = self.rooms[self.rooms.length - 1].id
+      })
+      .catch(function (error) {
+      })
+  },
+  methods: {
+    async createRoom() {
+      const self = this
+      await instance.post('/rooms', {
+        name: self.roomName
+      })
+        .then(function (response) {
+          self.rooms.push(response.data)
+          self.selectedRoomId = response.data.id
+          self.roomName = ''
+        })
+        .catch(function (error) {
+        })
+    }
+  },
+  computed: {
+
+  }
+})
